9f1de7f18b760f8f99de731a2b0d4ce55e91ca86
4jjnaomi <115657585+4jjnaomi@users.noreply.github.com>
2023-10-30 15:44:27 +0000

    Further data preparation

--- src/coursework1/data.py
+++ src/coursework1/data.py
@@ -1,6 +1,10 @@
 # Data preparation and understanding code
 from pathlib import Path
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 import pandas as pd
+
 
 def understand_df(dfdata):
     """Prints information that allows understanding of the DataFrame
@@ -19,8 +23,9 @@ def understand_df(dfdata):
     print("\nThe rows containing null values are: \n", dfdata_null_rows)
     dfdata_null_columns =dfdata.isnull().sum()
     print("\nThe number of null values in each column is: \n", dfdata_null_columns)
-
-    # TODO: Add code to check for unique values of region, country, category only has one category marker
+    #TODO: Explain the folowing two lines in pdf
+    print("The unique values in the column 'Country of HE provider' are: \n", dfdata['Country of HE provider'].unique())
+    print("The unique values in the column 'Region of HE provider' are: \n", dfdata['Region of HE provider'].unique())
 
 
 def prepare_df(dfdata):
@@ -46,6 +51,8 @@ def prepare_df(dfdata):
                                             'Table-4': 'Transport and environment',
                                             'Table-5': 'Finances and people'})
     print('\nThe columns of the prepared dataset are: \n', df_replaced.columns)
+    #TODO: Remove the percentage sign from the 'Value' column
+    #df_replaced.astype({'Value': 'float64'}).dtypes
 
     df_prepared = df_replaced
 
@@ -54,9 +61,22 @@ def prepare_df(dfdata):
 
     return df_prepared
 
+#Function including docstring that will explore the data by drawing plots to undertand if there are any outliers
+def explore_data(df_prepared):
+    """ The data is explored by drawing plots to understand if there are any outliers
+    
+    Args:
+        dfdata: A pandas DataFrame containing the data
+        
+        """
+    
+    df_prepared.groupby('Category marker')['Value'].plot(kind='box', subplots=True, layout=(3,3), sharex=False, sharey=False)
+    plt.show()
 
 if __name__ == "__main__":
     raw_data = Path(__file__).parent.joinpath('dataset','dataset.csv')
     data_df = pd.read_csv(raw_data, skiprows=10)
-    understand_df(data_df)
+    #understand_df(data_df)      
     cleaned_data = prepare_df(data_df)
+    explore_data(cleaned_data)
+
